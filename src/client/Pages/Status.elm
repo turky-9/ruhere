bb6c0ca535a2b15b091de6e73bbca96d5b0de935
ueno.yoshiharu <ueno.yoshiharu@jxgr.com>
2020-06-11 18:56:42 +0900

    todo: bootstrap

--- src/client/Pages/Status.elm
+++ src/client/Pages/Status.elm
@@ -19,6 +19,11 @@ type alias User =
     , status : EStatus
     }
 
+type alias RenderingUser =
+    { deptName : String
+    , user: User
+    }
+
 
 type alias Dept =
     { name : String
@@ -31,21 +36,28 @@ type Depts
     = Depts (List Dept)
 
 
-type alias Model =
-    Dept
+type alias CurrentDept =
+    String
+
+
+type Model
+    = Model Dept CurrentDept
 
 
 type Msg
     = MsgReload
     | MsgGotReload (Result Http.Error Dept)
+    | MsgChangeCurrentDept CurrentDept
 
 
-init : Dept
+init : Model
 init =
-    { name = "Init"
-    , users = []
-    , children = Depts []
-    }
+    Model
+        { name = "Init"
+        , users = []
+        , children = Depts []
+        }
+        "Init"
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -64,10 +76,13 @@ update msg model =
         MsgGotReload result ->
             case result of
                 Ok dept ->
-                    Debug.log ("@Status.update MsgGotReload Ok: " ++ Debug.toString dept) ( dept, Cmd.none )
+                    Debug.log ("@Status.update MsgGotReload Ok: " ++ Debug.toString dept) ( Model dept dept.name, Cmd.none )
 
                 Err err ->
                     Debug.log ("@Status.update MsgGotReload Err: " ++ Debug.toString err) ( model, Cmd.none )
+
+        MsgChangeCurrentDept curr ->
+            Debug.log ("@Status.update MsgChangeCurrentDept: " ++ Debug.toString curr) ( Model (getDept model) curr, Cmd.none )
 
 
 
@@ -78,59 +93,115 @@ update msg model =
 
 view : Model -> Html.Html Msg
 view model =
-    Html.div [ Attributes.style "display" "flex" ]
-        [ Html.div []
-            [ Html.button [ Events.onClick MsgReload ] [ Html.text "Reload" ]
-            , renderDept [ model ]
-            ]
-        , Html.div [] [ renderUsers model.users ]
-        ]
+    case model of
+        Model dept curr ->
+            Html.div [ Attributes.style "display" "flex" ]
+                [ Html.div []
+                    [ Html.button [ Events.onClick MsgReload ] [ Html.text "Reload" ]
+                    , renderDept [ dept ] curr
+                    ]
+                , Html.div [] [ renderUsers dept curr ]
+                ]
 
 
-renderDept : List Dept -> Html.Html msg
-renderDept depts =
+-- ############################################################################
+-- renderer
+-- ############################################################################
+renderDept : List Dept -> CurrentDept -> Html.Html Msg
+renderDept depts curr =
     case depts of
         x :: xs ->
-            Html.ul [] (renderDeptChildren depts [])
+            Html.ul [] (renderDeptChildren depts [] curr)
 
         [] ->
             Html.text ""
 
 
-renderDeptChildren : List Dept -> List (Html.Html msg) -> List (Html.Html msg)
-renderDeptChildren depts result =
+renderDeptChildren : List Dept -> List (Html.Html Msg) -> CurrentDept -> List (Html.Html Msg)
+renderDeptChildren depts result curr =
     case depts of
         x :: xs ->
-            renderDeptChildren xs (Html.li [] [ Html.text x.name, renderDept (getListDept x.children) ] :: result)
+            renderDeptChildren xs
+                (Html.li []
+                    [ Html.span
+                        [ Events.onClick (MsgChangeCurrentDept x.name)
+                        , Attributes.classList [("curr-dept", x.name == curr)]
+                        ]
+                        [ Html.text x.name ]
+                    , renderDept (getListDept x.children) curr
+                    ]
+                    :: result
+                )
+                curr
 
         [] ->
             List.reverse result
 
 
-getListDept : Depts -> List Dept
-getListDept d =
-    case d of
-        Depts x ->
-            x
-
-
-renderUsers : List User -> Html.Html msg
-renderUsers users =
+renderUsers : Dept -> CurrentDept -> Html.Html msg
+renderUsers top curr =
+    let
+        dept = searchCurrentDept top curr
+        users = (case dept of
+            Just x -> getFlatUserList [x]
+            Nothing -> [])
+                |> Debug.log ("@Status.renderUsers: current is " ++ curr ++ " and found is " ++ (Debug.toString dept))
+    in
     Html.table []
         (Html.tr []
-            [ Html.td [] [ Html.text "名前" ]
+            [ Html.td [] [ Html.text "部署" ]
+            , Html.td [] [ Html.text "名前" ]
             , Html.td [] [ Html.text "状態" ]
             ]
-            :: List.map renderUser users
+            :: List.map (renderUser curr) users
         )
 
 
-renderUser : User -> Html.Html msg
-renderUser user =
+renderUser : String -> User -> Html.Html msg
+renderUser dept user =
     Html.tr []
-        [ Html.td [] [ Html.text user.name ]
+        [ Html.td [] [ Html.text dept ]
+        , Html.td [] [ Html.text user.name ]
         , Html.td [] [ Html.text (displayStatus user.status) ]
         ]
+
+
+-- ############################################################################
+-- utils
+-- ############################################################################
+getFlatUserList : List Dept -> List User
+getFlatUserList depts =
+    case depts of
+        x :: xs ->
+            x.users ++ (getFlatUserList (getListDept x.children)) ++ (getFlatUserList xs)
+        [] ->
+            []
+
+searchCurrentDept : Dept -> CurrentDept -> Maybe Dept
+searchCurrentDept dept curr =
+    let
+        findChildren : List Dept -> Maybe Dept
+        findChildren depts =
+            case depts of
+                x :: xs ->
+                    let
+                        result = searchCurrentDept x curr
+                    in
+                    case result of
+                        Just _ -> result
+                        Nothing -> findChildren xs
+                [] -> Nothing
+    in
+    (
+    if dept.name == curr then
+        Just dept
+    else
+        getListDept dept.children |> findChildren
+    )
+    |> Debug.log ("@Status.searchCurrentDept: " ++ dept.name ++ " : " ++ curr)
+
+getRenderingUser : Dept -> List RenderingUser
+getRenderingUser dept = []
 
 
 displayStatus : EStatus -> String
@@ -145,7 +216,22 @@ displayStatus stat =
         Meeting ->
             "Meeting"
 
+getListDept : Depts -> List Dept
+getListDept d =
+    case d of
+        Depts x ->
+            x
 
+
+getDept : Model -> Dept
+getDept model =
+    case model of
+        Model d c ->
+            d
+
+-- ############################################################################
+-- decoder
+-- ############################################################################
 deptDecoder : Decode.Decoder Dept
 deptDecoder =
     Decode.map3 Dept
